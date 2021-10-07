abdbef0c8c63c40845646cdf476425a20362b6ef
Mikešová Tereza (211207) <xmikes13@vutbr.cz>
2021-10-07 14:25:30 +0200

    rekuze + iterace

--- rekurze.R
+++ rekurze.R
@@ -1,15 +1,24 @@
-jadro <- function(M,r,s){
+a <- matrix(c(3,0,0,0,1,4,0,0,5,3,0,0,1,2,6,7),byrow= TRUE, ncol=4)
+## rekurze
+cokolada <- function(M,r,s){
   
   if (r == nrow(M)){
     return(M[r,s])
   }
-  start <- M(M,r,s)
-  dolu <- M[r+1,s]
-  sikmo <- M[r+1,s+1]
-  max_coko <- max(dolu,sikmo)+start
+  else {
+    start <- M[r,s]
+    dolu <- M[r+1,s]
+    sikmo <- M[r+1,s+1]
+    max_coko <- max(dolu,sikmo)+start
+    #M[r+1,s] <- max(dolu,sikmo)+start
+  }
   return(max_coko)
 }
 
+
+
+
+### opsany
 cokorek <- function(M,r,s){
   L <- nrow(M)
   if (r==L){
@@ -21,4 +30,22 @@ cokorek <- function(M,r,s){
     csikmo <- cokorek(M,r+1,s+1)
   }
   
+}
+
+
+
+#####################
+#iterativnì
+cokoIter <- function(m){
+  s <- dim(m)
+  k1 <- seq(from=s[1]-1,to=1,by=-1)
+  for (r in k1){
+    k2 <- seq(from=r,to=1,by=-1)
+    for (s in k2){
+      Cdolu <- m[r+1,s]+m[r,s]
+      Csikmo <- m[r+1,s+1]+m[r,s]
+      m[r,s] <- max(c(Cdolu,Csikmo))
+    }
+  }
+  return(m[1,1])
 }
